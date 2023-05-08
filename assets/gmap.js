555439cd77ccbe2d1d01318717309c3a0ff5cd8f
monzuralam <monzur707@gmail.com>
2023-05-08 07:43:29 +0600

    map latitute & langitute has been dynamic

--- assets/gmap.js
+++ assets/gmap.js
@@ -5,8 +5,7 @@ window.marker = null;
 
 function initialize() {
     var map;
-
-    var nottingham = new google.maps.LatLng(51.507351, -0.127758);
+    var nottingham = new google.maps.LatLng( latitude, longitude );
 
     var style = [
         {
